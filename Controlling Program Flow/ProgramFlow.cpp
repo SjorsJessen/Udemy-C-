d5817fe0a2470106a185a974b2c205cebe42b7cd
Sjors Jessen <sjors.jessen@gmail.com>
2020-03-10 23:17:04 +0100

    While loops & Input Validation

--- Controlling Program Flow/ProgramFlow.cpp
+++ Controlling Program Flow/ProgramFlow.cpp
@@ -22,6 +22,8 @@ void ForLoop();
 
 void RangeBasedForLoop();
 
+void WhileLoop();
+
 int main()
 {
     //FillDictionary();
@@ -33,8 +35,66 @@ int main()
     //EnumSwitchStatement();
     //ConditionalOperator();
     //ForLoop();
-    RangeBasedForLoop();
+    //RangeBasedForLoop();
+    WhileLoop();
     return 0;
+}
+
+void WhileLoop()
+{
+//    int num{0};
+//    cout << "Enter a positive integer - start the countdown: ";
+//    cin >> num;
+//
+//    while(num > 0)
+//    {
+//        cout << num << endl;
+//        --num;
+//    }
+//    cout << "Blastoff!" << endl;
+
+//    int num{};
+//    cout << "Enter a positive integer to count up to: ";
+//    cin >> num;
+//
+//    int i{1};
+//    while(num >= i)
+//    {
+//        cout << i << endl;
+//        i++;
+//    }
+//
+//    int number{};
+//    cout << "Enter an integer less than 100: ";
+//    cin >> number;
+//
+//    while(number >= 100)
+//    {
+//        cout << "Enter an integer less than 100: ";
+//        cin >> number;
+//    }
+//
+//    cout << "Thanks" << endl;
+
+
+    bool done{false};
+    int number{0};
+
+    while(!done)
+    {
+        cout << "Enter an integer between 1 and 5";
+        cin >> number;
+
+        if(number <= 1 || number >= 5)
+        {
+            cout << "Out of range, try again" << endl;
+        }
+        else
+        {
+            cout << "Thanks!" << endl;
+            done = true;
+        }
+    }
 }
 
 void RangeBasedForLoop()
