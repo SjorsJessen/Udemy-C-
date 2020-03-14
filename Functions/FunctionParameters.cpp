fca4fa57013d2bd790a123205159e3f18f2e065e
Sjors Jessen <sjors.jessen@gmail.com>
2020-03-14 20:29:57 +0100

    Default Arguments
    Neat stuff!

--- Functions/FunctionParameters.cpp
+++ Functions/FunctionParameters.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
@@ -9,23 +10,20 @@ void pass_by_value2(string s);
 void pass_by_value3(vector<string> v);
 void pass_vector(vector<string> v);
 
+//Function Parameters
 void function_parameters();
-
 void pass_by_value1(int num)
 {
     num = 1000;
 }
-
 void pass_by_value2(string s)
 {
     s = "Changed";
 }
-
 void pass_by_value3(vector<string> v)
 {
     v.clear(); //Deletes all vector elements
 }
-
 void print_vector(vector<string> v)
 {
     for(auto value : v)
@@ -33,14 +31,44 @@ void print_vector(vector<string> v)
     cout << endl;
 }
 
+//Default Argument Values
+void default_argument_values();
+void greetings(string name, string prefix = "Mr.", string suffix = "");
+double calc_cost(double base_cost = 100.0, double tax_rate = 0.06, double shipping = 3.50);
+
+
 int main()
 {
-    function_parameters();
+    //function_parameters();
+    default_argument_values();
 
     cout << endl;
     return 0;
 }
 
+void default_argument_values()
+{
+    double cost{0};
+    cost = calc_cost(100.0,0.08,4.25); //Will use no default arguments
+
+    cout << fixed << setprecision(2);
+    cout << "Cost is: " << cost << endl;
+
+    cost = calc_cost(100.0, 0.08); //Will use default shipping. 100 + 8 + 4.25
+    cout << "Cost is: " << cost << endl;
+
+    cost = calc_cost(200.0); //Will use default tax and shipping. 200 + 12 + 3.50
+    cout << "Cost is: " << cost << endl;
+
+    cost = calc_cost();
+    cout << "Cost is: " << cost << endl; //100 + 6 + 3.50
+
+    greetings("Glenn Jones", "Dr.", "M.D");
+    greetings("James Rogers", "Professor", "Ph.D.");
+    greetings("Frank Miller", "Dr.");
+    greetings("William Smith");
+    greetings("Mary Howard", "Mrs.", "Ph.D.");
+}
 void function_parameters()
 {
     int num{10};
@@ -67,5 +95,13 @@ void function_parameters()
     cout << "\nstooges after calling pass_by_value3: ";
     print_vector(stooges);
 }
+void greetings(string name, string prefix, string suffix)
+{
+    cout << "Hello " << prefix + " " + name + " " + suffix << endl;
+}
 
+double calc_cost(double base_cost, double tax_rate, double shipping)
+{
+    return base_cost += (base_cost * tax_rate) + shipping;
+}
 
