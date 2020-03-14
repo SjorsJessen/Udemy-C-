36226322cc007a70b2e2c13bf656d958e3a6d00c
Sjors Jessen <sjors.jessen@gmail.com>
2020-03-14 15:32:01 +0100

    Random number calculations with Seed

--- Functions/Functions.cpp
+++ Functions/Functions.cpp
@@ -1,15 +1,41 @@
 #include <iostream>
 #include <cmath> //Required for math calculations
+#include <cstdlib> //Required for rand()
+#include <ctime> //Required for time)
 
 void MathCalculations();
+
+void RandomNumberCalculations();
 
 using namespace std;
 
 int main()
 {
-    MathCalculations();
-
+    //MathCalculations();
+    RandomNumberCalculations();
     return 0;
+}
+
+void RandomNumberCalculations()
+{
+    int random_number{};
+    size_t count{10}; //number of random numbers to generate
+    int min{1}; //lower_bound (inclusive)
+    int max{6}; //upper_bound (inclusive)
+
+    //Seed the random number generator
+    //If you don't seed the generator you will get the same sequence random numbers every run
+
+    cout << "RAND_MAX on my system is: " << RAND_MAX << endl;
+    srand(time(nullptr)); //Seeds the random number generator with time
+
+    for (size_t i {1}; i <= count; ++i)
+    {
+        random_number = rand() % max + min; //Generate a random number (min, max)
+        cout << random_number << endl;
+    }
+
+    cout << endl;
 }
 
 void MathCalculations()
