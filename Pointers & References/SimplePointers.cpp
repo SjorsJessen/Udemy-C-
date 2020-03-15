9cde7baa6339de5e153ddc61d6391e1b55b21c44
Sjors Jessen <sjors.jessen@gmail.com>
2020-03-16 00:43:16 +0100

    Pointer Arithmetic

--- Pointers & References/SimplePointers.cpp
+++ Pointers & References/SimplePointers.cpp
@@ -8,6 +8,8 @@ void dynamic_memory_allocation();
 
 void relationship_between_pointers_and_arrays();
 
+void pointer_arithmetic();
+
 using namespace std;
 
 int main()
@@ -15,9 +17,58 @@ int main()
     //store_address_in_pointer();
     //dereferencing_pointer();
     //dynamic_memory_allocation();
-    relationship_between_pointers_and_arrays();
+    //relationship_between_pointers_and_arrays();
+    pointer_arithmetic();
     cout << endl;
     return 0;
+}
+
+void pointer_arithmetic()
+{
+    int scores[]{100, 95, 89, 68, -1};
+    int *score_ptr{scores};
+
+    while(*score_ptr != -1)
+    {
+        cout << *score_ptr << endl;
+        score_ptr++;
+    }
+
+    cout << "\n-------------------------" << endl;
+    score_ptr = scores;
+    while(*score_ptr != -1)
+        cout << *score_ptr++ << endl; //Will generate same outcome as while loop above
+
+    cout << "\n-------------------------" << endl;
+    string s1{"Frank"};
+    string s2{"Frank"};
+    string s3{"James"};
+
+    string *p1{&s1};
+    string *p2{&s2};
+    string *p3{&s1};
+
+    cout << boolalpha;
+    cout << p1 << " == " << p2 << ": " << (p1 == p2) << endl; //false
+    cout << p1 << " == " << p3 << ": " << (p1 == p3) << endl; //true
+
+    cout << *p1 << " == " << *p2 << ": " << (*p1 == *p2) << endl; //true
+    cout << *p1 << " == " << *p2 << ": " << (*p1 == *p2) << endl; //true
+
+    p3 = &s3; //Point to James
+    cout << *p1 << " == " << *p3 << ": " << (*p1 == *p3) << endl; //false
+
+    cout << "\n----------------------------" << endl;
+    char name[]{"Frank"};
+
+    char *char_ptr1{nullptr};
+    char *char_ptr2{nullptr};
+
+    char_ptr1 = &name[0]; //F
+    char_ptr2 = &name[3]; //n
+
+    cout << "In the string " << name << ", " << *char_ptr2 << " is " << (char_ptr2 - char_ptr1) << " characters away from " << *char_ptr1 << endl;
+    cout << endl;
 }
 
 void relationship_between_pointers_and_arrays()
